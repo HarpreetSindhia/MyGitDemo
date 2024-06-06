fc9f46e8ce834a3c62144ebcc453d733903ad762
harpreetsindhia999@gmail.com <harpreetsindhia999@gmail.com>
2024-06-06 15:48:57 +0530

    New Commit from MyGitDemo Guy

---
+++ FirstSeleniumDesignFramework/src/test/java/rahulshettyacademy/TestComponents/BaseTest_2.java
@@ -0,0 +1,20 @@
+package rahulshettyacademy.TestComponents;
+
+import org.testng.annotations.Test;
+
+public class BaseTest_2 {
+	
+	@Test
+	public void AutomateWithSelenium() {
+		
+		System.out.println("Selenium is a pwerful tool for automating web applications");
+	}
+	
+	
+	@Test
+	public void TestNG() {
+		
+		System.out.println("Rinning testcases from Jenkins is quite easy");
+	}
+
+}
